224a52b8a9ea03695b93c1a68d88298490509a56
Ingram Weeks <ingram.weeks@gmail.com>
2020-12-28 16:55:21 +0000

    itoh updated and tested

--- 3-4/itob.c
+++ 3-4/itob.c
@@ -1,108 +1,164 @@
-#include<stdio.h>
+/*
+Code to test the itob function.
+This is my solultion to Exercise 3-3 of "The C Programming Language"
+by Brian W. Kernighan and Dennis M. Ritchie.
+*/
 
-#define MAX 1000
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 100
+#define LOWLIMIT -2147483648
+#define HIGHLIMIT 2147483647
 
 char s[MAX];
 
-void itoh(int n, char* s);
-void itob(unsigned n, char* s);
+// Function declaration
+int itoh(int n, char* s);
+int itob(int n, char* s);
 void reverse(char* s);
-
 
 int main(void)
 {
-  itoh(-2147483648, s);
-  puts(s);
-}
-
-void itob(unsigned n, char* s)
-{
-  int i;
-
-  i = 0;
-
-  do
+  // Convert an integer to a string representation
+  for(int i = LOWLIMIT; i <= HIGHLIMIT; i++)
   {
-    s[i++] = n % 2 + '0';
-  } while(((n /= 2) > 0));
+    itoh(i, s);
 
-  s[i] = '\0';
+    puts(s);
+  }
 
-  reverse(s);
+  return EXIT_SUCCESS;
 }
 
-void itoh(int n, char* s)
+// Integer argument is converted to a string representation
+int itoh(int n, char* s)
 {
   int i, sign;
+  int max = 1;
+  int temp = 1;
+
+  // Left shift to get max possible size for input int
+  for(i=0 ; temp>0; i++)
+  {
+    max = temp;
+    temp = (temp << 1) + 1;
+  }
+
+  // Check if input is greater in magnitude than max value for positive
+  // integer. If so, then return max negative.
+  if(((sign = n) < 0) && ((-max) > n))
+  {
+    sprintf(s, "-80000000");
+
+    return EXIT_SUCCESS;
+
+  } else if((sign = n) < 0)
+  {
+    n = -n;
+  }
 
   i = 0;
 
-  if((sign = n) < 0)
-  {
-    n = (~n);
-
-    s[i++] = n % 16 + '0' + 1;
-  }
-
-  while((n /= 16) > 0)
+  // Find remainder of division by 10 and add to ASCII code offset for
+  // numeric symbols.
+  // While there is still another order of magnitude, repeat the loop.
+  do
   {
     s[i++] = n % 16 + '0';
-  }
+  } while((n /= 16) > 0);
 
+  // If the number was negative, then add a hyphen to the array.
   if(sign < 0)
   {
     s[i++] = '-';
   }
-
+  // Add end of string character.
   s[i] = '\0';
 
   reverse(s);
 
-  // Replace ascii codes over 57 with A-F
-  for(int i=0; s[i]!='\0'; i++)
+  for(int i = 0; s[i]!='\0'; i++)
   {
     if(s[i] > 57)
     {
       s[i] += 7;
     }
   }
+
+  return EXIT_SUCCESS;
 }
 
+// Integer argument is converted to a string representation
+int itob(int n, char* s)
+{
+  int i, sign;
+  int max = 1;
+  int temp = 1;
 
+  // Left shift to get max possible size for input int
+  for(i=0 ; temp>0; i++)
+  {
+    max = temp;
+    temp = (temp << 1) + 1;
+  }
+
+  // Check if input is greater in magnitude than max value for positive
+  // integer. If so, then return max negative.
+  if(((sign = n) < 0) && ((-max) > n))
+  {
+    sprintf(s, "-10000000000000000000000000000000");
+
+    return EXIT_SUCCESS;
+
+  } else if((sign = n) < 0)
+  {
+    n = -n;
+  }
+
+  i = 0;
+
+  // Find remainder of division by 10 and add to ASCII code offset for
+  // numeric symbols.
+  // While there is still another order of magnitude, repeat the loop.
+  do
+  {
+    s[i++] = n % 2 + '0';
+  } while((n /= 2) > 0);
+
+  // If the number was negative, then add a hyphen to the array.
+  if(sign < 0)
+  {
+    s[i++] = '-';
+  }
+  // Add end of string character.
+  s[i] = '\0';
+
+  reverse(s);
+
+  return EXIT_SUCCESS;
+}
+
+// Reverse a string of input chars
 void reverse(char* s)
 {
   int i, j;
   char temp[MAX];
 
-  // Get string length i
-  puts("getting string length!");
-
-  for(i=0; s[i]!='\0'; i++)
-  {
-    putchar(s[i]);
-  }
+  // Get length of input string
+  for(i=0; s[i]!='\0'; i++);
 
   // Copy to reverse to temporary string
-
-  puts("\nreversing to temp string!");
   for(j=0; j<=i; j++)
   {
     temp[j] = s[i-(j+1)];
-
-    putchar(temp[j]);
   }
 
   temp[j] = '\0';
 
   // Copy reversed string back to original
-
-  puts("\nCopying reversed string! ");
-
   for(i=0; temp[i]!='\0'; i++)
   {
     s[i] = temp[i];
-
-    putchar(s[i]);
   }
-  puts("\nFunction finished!");
 }
