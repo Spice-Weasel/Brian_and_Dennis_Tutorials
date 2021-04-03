90e00d26a5944d39cca9953e724a08334669a361
Ingram Weeks <ingram.weeks@gmail.com>
2021-04-03 14:56:18 +0100

    tidied up reverse function, it now returns void - print statments removed

--- 4-8/main.c
+++ 4-8/main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
-int reverse(int i, char* s1, char* s2);
+
+void reverse(int i, char* s1, char* s2);
+
 
 int main(int argc, char* argv[])
 {
@@ -16,31 +18,29 @@ int main(int argc, char* argv[])
     printf("s2: %s\n", s2);
 }
 
-int reverse(int i, char* s1, char* s2)
+// reverse takes string s1 and puts the reversed string into s2
+void reverse(int i, char* s1, char* s2)
 {
-    static int idx;
     static int length;
     static int j;
 
+    // If the end of the string s1 has not been reached then call self and increment
+    // counter.
     if(s1[i]!='\0')
     {
         reverse(++i, s1, s2);
 
-        s2[length] = s1[j];
+        s2[length--] = s1[j++];
 
-        printf("s2[ %d ] = %d, s1[ %d ] = %d\n", length, s2[length], j, s1[j]);
-
-        length--;
-        j++;
-
+    // End of string has been reached, set length equal to counter value and
+    // set put null char at end of string.
     } else {
 
-        length = i;
-        s2[length] = '\0';
-        length--;
+        // assign values to static variables used for reversal
         j = 0;
-    }
-    printf("returned index i %d\n", i);
+        length = i;
 
-    return i;
+        s2[length--] = '\0';
+    }
+    return;
 }
